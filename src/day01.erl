e2fafabe7e9d7f0f6f7ab1518a61020c02790a24
Jesper Eskilson <jesper.eskilson@klarna.com>
2022-12-01 07:13:55 +0100

    Solution day1

--- src/day01.erl
+++ src/day01.erl
@@ -4,7 +4,24 @@
 
 -ifdef(TEST).
 
-part1_test() ->
-  ok.
+day01_test() ->
+  MostCals =
+    lists:reverse(
+      lists:sort(
+        lists:map(
+          fun(ElfCals) ->
+              lists:sum(
+                lists:filtermap(
+                  fun(<<>>) -> false;
+                     (Cal) -> {true, binary_to_integer(Cal)}
+                  end, binary:split(ElfCals, <<"\n">>, [global])))
+          end, binary:split(input:get(1), <<"\n\n">>, [global])))),
+
+  P1 = hd(MostCals),
+  ?assertEqual(69836, P1),
+
+  {TopThree, _} = lists:split(3, MostCals),
+  P2 = lists:sum(TopThree),
+  ?assertEqual(207968, P2).
 
 -endif.
