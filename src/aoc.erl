e3697acbf35d716a320970f7ed8e7b7b5cf795a0
Jesper Eskilson <jesper.eskilson@klarna.com>
2022-12-04 18:10:38 +0100

    optimizations of day 3 C solution

--- src/aoc.erl
+++ src/aoc.erl
@@ -18,7 +18,7 @@ timings() ->
   Days = [{day01, 5000},
           {day02, 5000},
           {day03, 3000},
-          {day04, 5000}],
+          {day04, 2000}],
   Str = "Day,Reps,Time (usecs),Time (msecs)\n" ++
     lists:map(fun({Day, Reps}) ->
                   {TimeUsecs, _} = measure(fun Day:solve/0, Reps),
--- src/day03.c
+++ src/day03.c
@@ -8,13 +8,13 @@
 #define handle_error(msg) \
   do { perror(msg); exit(EXIT_FAILURE); } while (0)
 
-int64_t now()
+void *do_mmap(const char *filename, int *fd, size_t *length);
+void do_munmap(int fd, void *addr, size_t length);
+
+static int64_t now()
 {
   struct timespec ts;
-
-  if (clock_gettime(CLOCK_REALTIME, &ts) != 0)
-    handle_error("clock_gettime");
-
+  clock_gettime(CLOCK_REALTIME, &ts);
   return ts.tv_sec * 1000000000 + ts.tv_nsec;
 }
 
@@ -23,14 +23,24 @@ typedef struct {
   int64_t p2;
 } solution_t;
 
-int prio(char c) {
+static int prio(char c) {
   if (c <= 'Z')
     return c - 'A' + 27;
   else
     return c - 'a' + 1;
 }
 
-int64_t str_to_mask(char *str, int offset, int len) {
+static int64_t str_to_mask(const char *str, int len)
+{
+  int64_t mask = 0;
+  for (int i = 0; i < len; i++) {
+    mask |= 1LL << prio(str[i]);
+  }
+  return mask;
+}
+
+static int64_t str_to_mask_with_offset(const char *str, int offset, int len)
+{
   int64_t mask = 0;
   for (int i = 0; i < len; i++) {
     mask |= 1LL << prio(str[i + offset]);
@@ -38,65 +48,71 @@ int64_t str_to_mask(char *str, int offset, int len) {
   return mask;
 }
 
-int mask_to_prio(int64_t mask) {
-  return __builtin_ctzll(mask);
-}
+// mask_to_prio() is essentially "count trailing zeroes", and gcc has
+// a built-in for that.
+#define mask_to_prio __builtin_ctzll
 
-int count1(char *str, int len) {
-  int half = len / 2;
-  int64_t left_mask = str_to_mask(str, 0, half);
-  int64_t right_mask = str_to_mask(str, half, half);
+static int count1(const char *str, int len)
+{
+  int half = len >> 1;
+  int64_t left_mask = str_to_mask(str, half);
+  int64_t right_mask = str_to_mask_with_offset(str, half, half);
   return mask_to_prio(left_mask & right_mask);
 }
 
-int count2(char *a, int alen, char *b, int blen, char *c, int clen) {
-  int64_t mask_a = str_to_mask(a, 0, alen);
-  int64_t mask_b = str_to_mask(b, 0, blen);
-  int64_t mask_c = str_to_mask(c, 0, clen);
+static int count2(const char *a, int alen, const char *b, int blen, const char *c, int clen)
+{
+  int64_t mask_a = str_to_mask(a, alen);
+  int64_t mask_b = str_to_mask(b, blen);
+  int64_t mask_c = str_to_mask(c, clen);
   return mask_to_prio(mask_a & mask_b & mask_c);
 }
 
 #define BUFSIZE 512
 
-void solve(const char *filename, solution_t *sol) {
-  size_t length;
-  FILE *f = fopen(filename, "r");
-  if (f == NULL)
-    handle_error("fopen");
+void solve(const char *addr, int length, solution_t *sol)
+{
+  const char *buf = addr;
 
   sol->p1 = 0;
   sol->p2 = 0;
 
   while (1) {
-    char buf1[BUFSIZE];
-    char buf2[BUFSIZE];
-    char buf3[BUFSIZE];
+    if (buf >= addr + length)
+      break;
 
-    char *p1 = fgets(buf1, BUFSIZE, f);
-    if (p1 == NULL) break;
+    const char *p1 = buf;
+    const char *p2 = strchr(buf, '\n') + 1;
+    const char *p3 = strchr(p2, '\n') + 1;
+    buf = strchr(p3, '\n') + 1;
 
-    char *p2 = fgets(buf2, BUFSIZE, f);
-    char *p3 = fgets(buf3, BUFSIZE, f);
-
-    int p1len = strlen(p1) - 1;
-    int p2len = strlen(p2) - 1;
-    int p3len = strlen(p3) - 1;
+    int p1len = p2 - p1 - 1;
+    int p2len = p3 - p2 - 1;
+    int p3len = buf - p3 - 1;
 
     sol->p1 += count1(p1, p1len) + count1(p2, p2len) + count1(p3, p3len);
     sol->p2 += count2(p1, p1len, p2, p2len, p3, p3len);
   }
-
-  fclose(f);
 }
 
-
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
   const char *filename = argv[1];
   solution_t sol = { 0 };
+  size_t length;
+  int fd;
+  char *addr = do_mmap(filename, &fd, &length);
+
+  int reps = 10000;
   int64_t t0 = now();
-  solve(filename, &sol);
+  for (int i = 0; i < reps; i++) {
+    solve(addr, length, &sol);
+  }
   int64_t t1 = now();
+
   printf("p1 = %ld, p2 = %ld, time = %.2f usecs\n",
-         sol.p1, sol.p2, ((double)(t1 - t0)) / 1000);
+         sol.p1, sol.p2, (((double)(t1 - t0)) / 1000) / reps);
+
+  do_munmap(fd, addr, length);
   exit(EXIT_SUCCESS);
 }
