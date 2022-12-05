acb4a33378662b2918d1b1a90c7f51114d24dfe8
Jesper Eskilson <jesper.eskilson@klarna.com>
2022-12-05 07:04:44 +0100

    day 5 cleanup

--- src/day05.erl
+++ src/day05.erl
@@ -5,69 +5,52 @@
 
 -include_lib("eunit/include/eunit.hrl").
 
--compile([export_all, nowarn_export_all]).
-
-%%                         [Z] [W] [Z]
-%%         [D] [M]         [L] [P] [G]
-%%     [S] [N] [R]         [S] [F] [N]
-%%     [N] [J] [W]     [J] [F] [D] [F]
-%% [N] [H] [G] [J]     [H] [Q] [H] [P]
-%% [V] [J] [T] [F] [H] [Z] [R] [L] [M]
-%% [C] [M] [C] [D] [F] [T] [P] [S] [S]
-%% [S] [Z] [M] [T] [P] [C] [D] [C] [D]
-%%  1   2   3   4   5   6   7   8   9
-
 initial_state() ->
-  {[n, v, c, s],
-   [s, n, h, j, m, z],
-   [d, n, j, g, t, c, m],
-   [m, r, w, j, f, d, t],
-   [h, f, p],
-   [j, h, z, t, c],
-   [z, l, s, f, q, r, p, d],
-   [w, p, f, d, h, l, s, c],
-   [z, g, n, f, p, m, s, d]}.
+  #{1 => [n, v, c, s],
+    2 => [s, n, h, j, m, z],
+    3 => [d, n, j, g, t, c, m],
+    4 => [m, r, w, j, f, d, t],
+    5 => [h, f, p],
+    6 => [j, h, z, t, c],
+    7 => [z, l, s, f, q, r, p, d],
+    8 => [w, p, f, d, h, l, s, c],
+    9 => [z, g, n, f, p, m, s, d]}.
 
 solve() ->
   Bin = input:get(5),
   Lines = binary:split(Bin, <<"\n">>, [global]),
-  State1 = initial_state(),
-  State2 = initial_state(),
-  move_crates(Lines, {State1, State2}).
+  Stacks1 = initial_state(),
+  Stacks2 = initial_state(),
+  move_crates(Lines, {Stacks1, Stacks2}).
 
-move_crates(Lines, States) ->
-  {P1, P2} =
+move_crates(Lines, Stacks) ->
+  {StacksOut1, StacksOut2} =
     lists:foldl(
-      fun(<<"move ", Rest/binary>>, {State1, State2}) ->
+      fun(<<"move ", Rest/binary>>, {Stacks1, Stacks2}) ->
           [WhatBin, <<"from">>, FromBin, <<"to">>, ToBin] =
             binary:split(Rest, <<" ">>, [global]),
           What = binary_to_integer(WhatBin),
           From = binary_to_integer(FromBin),
           To = binary_to_integer(ToBin),
-          {move(What, From, To, State1, fun lists:reverse/1),
-           move(What, From, To, State2, fun(L) -> L end)};
-         (_, StateIn) ->
-          StateIn
-      end, States, Lines),
-  {top_crates(P1), top_crates(P2)}.
+          {move(What, From, To, Stacks1, fun lists:reverse/1),
+           move(What, From, To, Stacks2, fun(L) -> L end)};
+         (_, StacksAcc) ->
+          StacksAcc
+      end, Stacks, Lines),
+  {top_crates(StacksOut1), top_crates(StacksOut2)}.
 
 top_crates(Stacks) ->
   lists:flatten(
     lists:map(
-      fun(Stack) ->
+      fun({_, Stack}) ->
           atom_to_list(hd(Stack))
       end,
-      tuple_to_list(Stacks))).
+      lists:sort(maps:to_list(Stacks)))).
 
-b2i(N) ->
-   binary_to_integer(N).
-
-move(What, From, To, State, Fun) ->
-  FromStack = element(From, State),
-  ToStack = element(To, State),
+move(What, From, To, Stacks, Fun) ->
+  #{From := FromStack, To := ToStack} = Stacks,
   {Crates, Remaining} = lists:split(What, FromStack),
-  setelement(To, setelement(From, State, Remaining),
-             Fun(Crates) ++ ToStack).
+  Stacks#{From := Remaining, To := Fun(Crates) ++ ToStack}.
 
 -ifdef(TEST).
 
