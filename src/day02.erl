189267dc2f5fbc49f4a3d2b79960daa8ea121697
Jesper Eskilson <jesper.eskilson@klarna.com>
2022-12-11 19:10:12 +0100

    day 2 optimization

--- src/day02.erl
+++ src/day02.erl
@@ -3,97 +3,48 @@
 -export([solve/0]).
 -include_lib("eunit/include/eunit.hrl").
 
--define(OPPONENT_ROCK, $A).
--define(OPPONENT_PAPER, $B).
--define(OPPONENT_SCISSORS, $C).
+-define(O_ROCK, $A).
+-define(O_PAPER, $B).
+-define(O_SCISSORS, $C).
 
--define(PLAYER_ROCK, $X).
--define(PLAYER_PAPER, $Y).
--define(PLAYER_SCISSORS, $Z).
+-define(ROCK_OR_LOSE, $X).
+-define(PAPER_OR_DRAW, $Y).
+-define(SCISSORS_OR_WIN, $Z).
 
--define(EXPECTED_TO_LOSE, $X).
--define(EXPECTED_TO_DRAW, $Y).
--define(EXPECTED_TO_WIN, $Z).
+-define(LOSE, 0).
+-define(DRAW, 3).
+-define(WIN, 6).
 
-
-%% meaning of X/Y/Z in part 1
--type player_choice() :: ?PLAYER_ROCK |
-                         ?PLAYER_PAPER |
-                         ?PLAYER_SCISSORS.
-
-%% meaning of X/Y/Z in part 2
--type expected_outcome() :: ?EXPECTED_TO_DRAW |
-                            ?EXPECTED_TO_WIN |
-                            ?EXPECTED_TO_LOSE.
-
--type opponent_choice() :: ?OPPONENT_PAPER |
-                           ?OPPONENT_ROCK |
-                           ?OPPONENT_SCISSORS.
--type score() :: 1..3.
--type outcome_score() :: 0 | 3 | 6.
--type round_score() :: 1..9.
--type outcome() :: draw | player | opponent.
-
--spec score(Choice :: player_choice()) -> score().
-score(?PLAYER_ROCK) -> 1;
-score(?PLAYER_PAPER) -> 2;
-score(?PLAYER_SCISSORS) -> 3.
-
--spec outcome(OpponentChoice :: opponent_choice(),
-              PlayerChoice :: player_choice()) ->
-        outcome().
-outcome(?OPPONENT_ROCK, ?PLAYER_ROCK)         -> draw;
-outcome(?OPPONENT_ROCK, ?PLAYER_PAPER)        -> player;
-outcome(?OPPONENT_ROCK, ?PLAYER_SCISSORS)     -> opponent;
-
-outcome(?OPPONENT_PAPER, ?PLAYER_ROCK)        -> opponent;
-outcome(?OPPONENT_PAPER, ?PLAYER_PAPER)       -> draw;
-outcome(?OPPONENT_PAPER, ?PLAYER_SCISSORS)    -> player;
-
-outcome(?OPPONENT_SCISSORS, ?PLAYER_ROCK)     -> player;
-outcome(?OPPONENT_SCISSORS, ?PLAYER_PAPER)    -> opponent;
-outcome(?OPPONENT_SCISSORS, ?PLAYER_SCISSORS) -> draw.
-
--spec outcome_score(Outcome :: outcome() | expected_outcome()) -> outcome_score().
-outcome_score(opponent) -> 0;
-outcome_score(draw) -> 3;
-outcome_score(player) -> 6;
-outcome_score(?EXPECTED_TO_LOSE) -> 0;
-outcome_score(?EXPECTED_TO_DRAW) -> 3;
-outcome_score(?EXPECTED_TO_WIN) -> 6.
-
--spec score_part1(OpponentChoice :: opponent_choice(),
-                  PlayerChoice :: player_choice()) -> round_score().
-score_part1(OpponentChoice, PlayerChoice) ->
-  score(PlayerChoice) + outcome_score(outcome(OpponentChoice, PlayerChoice)).
-
--spec score_part2(OpponentChoice :: opponent_choice(),
-                  ExpectedOutcome :: expected_outcome()) -> round_score().
-score_part2(Opp, ExpectedOutcome) ->
-  PlayerChoice =
-    case ExpectedOutcome of
-      ?EXPECTED_TO_DRAW when Opp =:= ?OPPONENT_ROCK ->     ?PLAYER_ROCK;
-      ?EXPECTED_TO_DRAW when Opp =:= ?OPPONENT_PAPER ->    ?PLAYER_PAPER;
-      ?EXPECTED_TO_DRAW when Opp =:= ?OPPONENT_SCISSORS -> ?PLAYER_SCISSORS;
-
-      ?EXPECTED_TO_WIN  when Opp =:= ?OPPONENT_ROCK ->     ?PLAYER_PAPER;
-      ?EXPECTED_TO_WIN  when Opp =:= ?OPPONENT_PAPER ->    ?PLAYER_SCISSORS;
-      ?EXPECTED_TO_WIN  when Opp =:= ?OPPONENT_SCISSORS -> ?PLAYER_ROCK;
-
-      ?EXPECTED_TO_LOSE when Opp =:= ?OPPONENT_ROCK ->     ?PLAYER_SCISSORS;
-      ?EXPECTED_TO_LOSE when Opp =:= ?OPPONENT_PAPER ->    ?PLAYER_ROCK;
-      ?EXPECTED_TO_LOSE when Opp =:= ?OPPONENT_SCISSORS -> ?PLAYER_PAPER
-    end,
-  score(PlayerChoice) + outcome_score(ExpectedOutcome).
+-define(S_ROCK, 1).
+-define(S_PAPER, 2).
+-define(S_SCISSORS, 3).
 
 solve() ->
   Bin = input:get(2),
   do_solve(Bin, {0, 0}).
 
 do_solve(<<>>, Acc) -> Acc;
-do_solve(<<Opp:8, _:8, Pl:8, _:8, Rest/binary>>, {Score1, Score2}) when is_binary(Rest) ->
-  do_solve(Rest, {Score1 + score_part1(Opp, Pl),
-                  Score2 + score_part2(Opp, Pl)}).
+
+do_solve(<<?O_ROCK:8, _:8, ?ROCK_OR_LOSE:8, _:8, Rest/binary>>, {P1, P2}) when is_binary(Rest) ->
+  do_solve(Rest, {P1 + ?S_ROCK + ?DRAW, P2 + ?S_SCISSORS + ?LOSE});
+do_solve(<<?O_ROCK:8, _:8, ?PAPER_OR_DRAW:8, _:8, Rest/binary>>, {P1, P2}) when is_binary(Rest) ->
+  do_solve(Rest, {P1 + ?S_PAPER + ?WIN, P2 + ?S_ROCK + ?DRAW});
+do_solve(<<?O_ROCK:8, _:8, ?SCISSORS_OR_WIN:8, _:8, Rest/binary>>, {P1, P2}) when is_binary(Rest) ->
+  do_solve(Rest, {P1 + ?S_SCISSORS + ?LOSE, P2 + ?S_PAPER + ?WIN});
+
+do_solve(<<?O_PAPER:8, _:8, ?ROCK_OR_LOSE:8, _:8, Rest/binary>>, {P1, P2}) when is_binary(Rest) ->
+  do_solve(Rest, {P1 + ?S_ROCK + ?LOSE, P2 + ?S_ROCK + ?LOSE});
+do_solve(<<?O_PAPER:8, _:8, ?PAPER_OR_DRAW:8, _:8, Rest/binary>>, {P1, P2}) when is_binary(Rest) ->
+  do_solve(Rest, {P1 + ?S_PAPER + ?DRAW, P2 + ?S_PAPER + ?DRAW});
+do_solve(<<?O_PAPER:8, _:8, ?SCISSORS_OR_WIN:8, _:8, Rest/binary>>, {P1, P2}) when is_binary(Rest) ->
+  do_solve(Rest, {P1 + ?S_SCISSORS + ?WIN, P2 + ?S_SCISSORS + ?WIN});
+
+do_solve(<<?O_SCISSORS:8, _:8, ?ROCK_OR_LOSE:8, _:8, Rest/binary>>, {P1, P2}) when is_binary(Rest) ->
+  do_solve(Rest, {P1 + ?S_ROCK + ?WIN, P2 + ?S_PAPER + ?LOSE});
+do_solve(<<?O_SCISSORS:8, _:8, ?PAPER_OR_DRAW:8, _:8, Rest/binary>>, {P1, P2}) when is_binary(Rest) ->
+  do_solve(Rest, {P1 + ?S_PAPER + ?LOSE, P2 + ?S_SCISSORS + ?DRAW});
+do_solve(<<?O_SCISSORS:8, _:8, ?SCISSORS_OR_WIN:8, _:8, Rest/binary>>, {P1, P2}) when is_binary(Rest) ->
+  do_solve(Rest, {P1 + ?S_SCISSORS + ?DRAW, P2 + ?S_ROCK + ?WIN}).
 
 -ifdef(TEST).
 
