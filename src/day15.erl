07123aee00f686d24e0007a64938cf250c0d2052
Jesper Eskilson <jesper.eskilson@klarna.com>
2022-12-15 16:31:18 +0100

    day 15 cleanup

--- src/day15.erl
+++ src/day15.erl
@@ -25,70 +25,110 @@
 %% enormously.
 %%
 
-solve() ->
-  %% Bin = input:get(15),
-  %% P1YRange = {10, 10},
-  %% P2Range = {0, 20},
+-type coord() :: {integer(), integer()}.
 
-  Bin = <<"Sensor at x=2, y=18: closest beacon is at x=-2, y=15\n",
-          "Sensor at x=9, y=16: closest beacon is at x=10, y=16\n",
-          "Sensor at x=13, y=2: closest beacon is at x=15, y=3\n",
-          "Sensor at x=12, y=14: closest beacon is at x=10, y=16\n",
-          "Sensor at x=10, y=20: closest beacon is at x=10, y=16\n",
-          "Sensor at x=14, y=17: closest beacon is at x=10, y=16\n",
-          "Sensor at x=8, y=7: closest beacon is at x=2, y=10\n",
-          "Sensor at x=2, y=0: closest beacon is at x=2, y=10\n",
-          "Sensor at x=0, y=11: closest beacon is at x=2, y=10\n",
-          "Sensor at x=20, y=14: closest beacon is at x=25, y=17\n",
-          "Sensor at x=17, y=20: closest beacon is at x=21, y=22\n",
-          "Sensor at x=16, y=7: closest beacon is at x=15, y=3\n",
-          "Sensor at x=14, y=3: closest beacon is at x=15, y=3\n",
-          "Sensor at x=20, y=1: closest beacon is at x=15, y=3\n"
-        >>,
-  P1YRange = {10, 10},
-  P2Range = {0, 20},
+-record(snb, { sensor :: coord()
+             , beacon :: coord()
+             , dist :: integer()
+             }).
+
+-type snb() :: #snb{}.
+
+-record(input, { snbs = [] :: [snb()]
+               , items_on_p1_yline = sets:new()
+               }).
+
+solve() ->
+  InputType = actual,
+
+  {Bin, P1Y, P2Range} =
+    case InputType of
+      example ->
+        {<<"Sensor at x=2, y=18: closest beacon is at x=-2, y=15\n",
+           "Sensor at x=9, y=16: closest beacon is at x=10, y=16\n",
+           "Sensor at x=13, y=2: closest beacon is at x=15, y=3\n",
+           "Sensor at x=12, y=14: closest beacon is at x=10, y=16\n",
+           "Sensor at x=10, y=20: closest beacon is at x=10, y=16\n",
+           "Sensor at x=14, y=17: closest beacon is at x=10, y=16\n",
+           "Sensor at x=8, y=7: closest beacon is at x=2, y=10\n",
+           "Sensor at x=2, y=0: closest beacon is at x=2, y=10\n",
+           "Sensor at x=0, y=11: closest beacon is at x=2, y=10\n",
+           "Sensor at x=20, y=14: closest beacon is at x=25, y=17\n",
+           "Sensor at x=17, y=20: closest beacon is at x=21, y=22\n",
+           "Sensor at x=16, y=7: closest beacon is at x=15, y=3\n",
+           "Sensor at x=14, y=3: closest beacon is at x=15, y=3\n",
+           "Sensor at x=20, y=1: closest beacon is at x=15, y=3\n">>,
+         10, {0, 20}};
+      actual ->
+        {input:get(15), 2000000, {0, 4000000}}
+    end,
 
   Lines = binary:split(Bin, <<"\n">>, [global]),
-  SnB =
+  Input =
     lists:foldl(
       fun(<<>>, Acc) -> Acc;
-         (Line, Acc) ->
+         (Line, #input{ snbs = SnBs
+                      , items_on_p1_yline = P1Items
+                      } = Input)->
           case re:run(Line, "Sensor at x=(.*), y=(.*): closest beacon is at x=(.*), y=(.*)",
                       [{capture, all_but_first, binary}]) of
             {match, [Sx, Sy, Bx, By]} ->
               Sensor = {?int(Sx), ?int(Sy)},
               Beacon = {?int(Bx), ?int(By)},
               Dist = dist(Sensor, Beacon),
-              [{Sensor, Beacon, Dist}|Acc]
+              Input#input{ snbs = [#snb{ sensor = Sensor
+                                       , beacon = Beacon
+                                       , dist = Dist
+                                       }|SnBs]
+                         , items_on_p1_yline =
+                             case {Sensor, Beacon} of
+                               {{_, P1Y}, {_, P1Y}} ->
+                                 sets:add_element(Beacon, sets:add_element(Sensor, P1Items));
+                               {{_, P1Y}, _} ->
+                                 sets:add_element(Sensor, P1Items);
+                               {_, {_, P1Y}} ->
+                                 sets:add_element(Beacon, P1Items);
+                               _ ->
+                                 P1Items
+                             end
+                         }
           end
-      end, [], Lines),
+      end, #input{}, Lines),
 
-  P1XRange = {lists:min([Sx - Dist || {{Sx, _}, _, Dist} <- SnB]),
-              lists:max([Sx + Dist || {{Sx, _}, _, Dist} <- SnB])},
-  P1Cands = candidates(SnB, P1XRange, P1YRange),
+  {part1(Input, P1Y),
+   part2(Input, P2Range)}.
 
-  P1 = lists:foldl(fun(Pos, Acc) ->
-                       case is_in_range_of_any_sensor(Pos, SnB) of
-                         true -> [Pos|Acc];
-                         false -> Acc
+part1(Input, P1Y) ->
+  {P1XMin, P1XMax} = {lists:min([Sx - Dist || #snb{sensor = {Sx, _}, dist = Dist} <- Input#input.snbs]),
+                      lists:max([Sx + Dist || #snb{sensor = {Sx, _}, dist = Dist} <- Input#input.snbs])},
+  P1 = lists:foldl(fun(X, Acc) ->
+                       Pos = {X, P1Y},
+                       case sets:is_element(Pos, Input#input.items_on_p1_yline) of
+                         true -> Acc;
+                         _ ->
+                           case is_in_range_of_any_sensor(Pos, Input#input.snbs) of
+                             true -> Acc + 1;
+                             false -> Acc
+                           end
                        end
-                   end, [], P1Cands),
+                   end, 0, lists:seq(P1XMin, P1XMax)),
+  P1.
 
-  P2Cands = candidates(SnB, P2Range, P2Range),
+part2(Input, P2Range) ->
+  P2Cands = candidates(Input, P2Range),
   {value, Pos} = lists:search(fun(Pos) ->
                                   not lists:any(
-                                        fun({S, _, Dist}) ->
+                                        fun(#snb{sensor = S, dist = Dist}) ->
                                             dist(Pos, S) =< Dist
-                                        end, SnB)
+                                        end, Input#input.snbs)
                               end, P2Cands),
   P2 = tuning_frequency(Pos),
-  {length(P1), P2}.
+  P2.
 
 is_in_range_of_any_sensor(Pos, SnB) ->
-  lists:any(fun({S, _B, Dist}) ->
+  lists:any(fun(#snb{sensor = S, dist = Dist}) ->
                 case dist(Pos, S) of
-                  D when D =< Dist ->
-                    true;
+                  D when D =< Dist -> true;
                   _ -> false
                 end
             end, SnB).
@@ -96,32 +136,32 @@ is_in_range_of_any_sensor(Pos, SnB) ->
 tuning_frequency({X, Y}) ->
   X * 4000000 + Y.
 
-candidates(SnB, XRange, YRange) ->
-  lists:foldl(fun({S, _B, Dist}, Acc) ->
-                  just_outside(S, Dist, XRange, YRange, Acc)
-              end, [], SnB).
+candidates(Input, Range) ->
+  lists:foldl(fun(#snb{sensor = S, dist = Dist}, Acc) ->
+                  just_outside(S, Dist, Range, Range, Acc)
+              end, [], Input#input.snbs).
 
 in_range(X, {Min, Max}) ->
   X >= Min andalso X =< Max.
 
 %% Generate the coordinates with manhattan distance D + 1 from {Cx,
 %% Cy}.
-just_outside({Cx, Cy} = _Center, D, XRange, YRange, Acc) ->
+just_outside({Cx, Cy} = _Center, D, Range, Range, Acc) ->
   Dist = D + 1,
-  L1 = diagonal({Cx,        Cy - Dist}, {1,   1}, Dist, XRange, YRange, Acc),
-  L2 = diagonal({Cx + Dist, Cy},        {-1,  1}, Dist, XRange, YRange, L1),
-  L3 = diagonal({Cx,        Cy + Dist}, {-1, -1}, Dist, XRange, YRange, L2),
-  L  = diagonal({Cx - Dist, Cy},        {1,  -1}, Dist, XRange, YRange, L3),
+  L1 = diagonal({Cx,        Cy - Dist}, {1,   1}, Dist, Range, Acc),
+  L2 = diagonal({Cx + Dist, Cy},        {-1,  1}, Dist, Range, L1),
+  L3 = diagonal({Cx,        Cy + Dist}, {-1, -1}, Dist, Range, L2),
+  L  = diagonal({Cx - Dist, Cy},        {1,  -1}, Dist, Range, L3),
   L.
 
-diagonal(_, _, 0, _, _, Acc) ->
+diagonal(_, _, 0, _, Acc) ->
   Acc;
-diagonal({X, Y}, {Dx, Dy} = Delta, N, XRange, YRange, Acc) ->
-  case in_range(X, XRange) andalso in_range(Y, YRange) of
+diagonal({X, Y}, {Dx, Dy} = Delta, N, Range, Acc) ->
+  case in_range(X, Range) andalso in_range(Y, Range) of
     true ->
-      diagonal({X + Dx, Y + Dy}, Delta, N - 1, XRange, YRange, [{X, Y}|Acc]);
+      diagonal({X + Dx, Y + Dy}, Delta, N - 1, Range, [{X, Y}|Acc]);
     false ->
-      diagonal({X + Dx, Y + Dy}, Delta, N - 1, XRange, YRange, Acc)
+      diagonal({X + Dx, Y + Dy}, Delta, N - 1, Range, Acc)
   end.
 
 dist({X0, Y0}, {X1, Y1}) ->
@@ -136,7 +176,9 @@ print_coords(L) ->
 
 -ifdef(TEST).
 
-day13_test() ->
-  not_solved = solve().
+day13_test_() ->
+  {timeout, 20, fun() ->
+                    {4665948, 13543690671045} = solve()
+                end}.
 
 -endif.
