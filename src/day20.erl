54e1b4d8b82adea3fe0e1a806c5400ccbb1e3061
Jesper Eskilson <jesper.eskilson@klarna.com>
2023-10-29 16:03:33 +0100

    day20 optimizations

--- src/day20.erl
+++ src/day20.erl
@@ -14,76 +14,64 @@ solve() ->
           [binary_to_integer(Bin)|Acc]
       end, [], binary:split(input:get(20), <<"\n">>, [global])),
 
-  %% Part 1
-  {Queue, List} = make_queue(Numbers),
-  Queue1 = mix(Queue, List),
-  P1 = sum3(Queue1),
+  {mix(Numbers, 1, 1),
+   mix(Numbers, ?ENCRYPTION_KEY, 10)}.
 
-  %% Part 2
-  Numbers2 = lists:map(fun(N) -> N * ?ENCRYPTION_KEY end, Numbers),
-  {Queue2, List2} = make_queue(Numbers2),
-  Queue3 = lists:foldl(
-             fun(_, Q) ->
-                 mix(Q, List2)
-             end, Queue2, lists:seq(1, 10)),
-  P2 = sum3(Queue3),
-  {P1, P2}.
+sum3(Ring, Zero) ->
+  R1 = ring:move_element_to_head(Zero, Ring),
+  {Sum, _} =
+    lists:foldl(fun(_, {Sum, R}) ->
+                    R2 = ring:shift(-1000, R),
+                    {_, A} = ring:peek(R2),
+                    {A + Sum, R2}
+                end, {0, R1}, [1, 2, 3]),
+  Sum.
 
-
-sum3(Queue) ->
-  Q0 = rotate_number_to_front(0, Queue),
-  List = queue:to_list(Q0),
-  sum3(0, List, []).
-
-sum3(_, [], Acc) -> Acc;
-sum3(N, [A|List], Acc) when (N == 1000) orelse (N == 2000) orelse (N == 3000) ->
-  sum3(N + 1, List, [A|Acc]);
-sum3(N, [_|List], Acc) ->
-  sum3(N + 1, List, Acc).
-
-make_queue(Numbers) ->
+mix(Numbers, Multiplier, Rounds) ->
   Len = length(Numbers),
-  List = lists:zip(lists:seq(0, Len - 1), Numbers),
-  {queue:from_list(List), List}.
+  List = lists:zip(lists:seq(0, Len - 1),
+                   lists:map(fun(N) -> N * Multiplier end, Numbers)),
+  Ring = ring:from_list(List),
+  do_mix_rounds(Ring, List, Rounds).
 
-mix(Queue, List) ->
-  Len = length(List),
-  lists:foldl(
-    fun({Idx, Num}, QueueIn) ->
-        Q1 = rotate_index_to_front(Idx, QueueIn),
-        {{value, Head}, Q2} = queue:out(Q1),
-        Shift = (Num + (Len - 1)) rem (Len - 1),
-        Q3 = rotate(-Shift, Q2),
-        Q4 = queue:in(Head, Q3),
-        Q4
-      end, Queue, List).
-
-rotate_index_to_front(Idx, Queue) ->
-  case queue:head(Queue) of
-    {Idx0, _} when Idx == Idx0 ->  Queue;
-    _ -> rotate_index_to_front(Idx, rotate(-1, Queue))
-  end.
-
-rotate_number_to_front(Num, Queue) ->
-  case queue:head(Queue) of
-    {_, N} when N == Num ->  Queue;
-    _ -> rotate_number_to_front(Num, rotate(-1, Queue))
-  end.
-
-%% If viewing the queue with the head to the left and the tail to the
-%% right, rotating by a negative amount means leftwards, i.e. popping
-%% the head and inserting it at the tail, and vice versa.
-rotate(0, Queue) -> Queue;
-rotate(N, Queue) when N > 0 ->
-  {{value, Rear}, Q0} = queue:out_r(Queue),
-  Q1 = queue:in_r(Rear, Q0),
-  rotate(N - 1, Q1);
-rotate(N, Queue) when N < 0 ->
-  {{value, Head}, Q0} = queue:out(Queue),
-  Q1 = queue:in(Head, Q0),
-  rotate(N + 1, Q1).
+do_mix_rounds(Ring, List, 0) ->
+  {value, Zero} = lists:search(fun({_, 0}) -> true;
+                                  (_) -> false
+                               end, List),
+  sum3(Ring, Zero);
+do_mix_rounds(Ring, List, N) ->
+  Len = ring:length(Ring),
+  RingOut =
+    lists:foldl(
+      fun({_, Num} = Elem, RingIn) ->
+          Ring0 = ring:move_element_to_head(Elem, RingIn),
+          {_, Ring1} = ring:remove(Ring0),
+          Shift = (Num + (Len - 1)) rem (Len - 1),
+          Ring2 = ring:shift(-Shift, Ring1),
+          Ring3 = ring:insert(Elem, Ring2),
+          Ring3
+      end, Ring, List),
+  do_mix_rounds(RingOut, List, N - 1).
 
 -ifdef(TEST).
+
+%% mix_test() ->
+%%   Numbers = [1, 2, -3, 3, -2, 0, 4],
+%%   {Ring, List} = make_ring(Numbers),
+%%   Ring1 = mix(Ring, List),
+%%   Ring2 = ring:move_element_to_head({0, 1}, Ring1),
+%%   {_, L} = lists:unzip(ring:to_list(Ring2)),
+%%   ?assertEqual([1, 2, -3, 4, 0, 3, -2], L).
+
+%% sum3_test() ->
+%%   L = [1, 2, -3, 4, 0, 3, -2],
+%%   {Ring, List} = make_ring(L),
+%%   {value, Zero} = lists:search(fun({_, 0}) -> true;
+%%                                   (_) -> false
+%%                                end, List),
+%%   ?debugVal(Zero),
+%%   ?debugVal(Ring),
+%%   ?assertEqual(3, sum3(Ring, Zero)).
 
 day20_test_() ->
   {timeout, 600, ?_assertEqual({7278, 14375678667089}, solve())}.
---
+++ src/ring.erl
@@ -0,0 +1,115 @@
+-module(ring).
+
+-export([new/0,
+         from_list/1,
+         to_list/1,
+         shift/2,
+         insert/2,
+         remove/1,
+         peek/1,
+         move_element_to_head/2,
+         length/1
+        ]).
+
+-include_lib("eunit/include/eunit.hrl").
+
+-record(ring, { head = 0 :: integer()
+              , length = 0 :: integer()
+              , elements = [] :: list(term())
+              }).
+
+-opaque ring() :: #ring{}.
+
+-export_type([ring/0]).
+
+-spec new() -> ring().
+new() ->
+  from_list([]).
+
+-spec from_list(List :: list()) -> ring().
+from_list(List) ->
+  #ring{head = 0, length = erlang:length(List), elements = List}.
+
+%% Convert the ring into a list where the first elements in the list
+%% is the head of the ring.
+-spec to_list(Ring :: ring()) -> list().
+to_list(#ring{head = Head, elements = Elements}) ->
+  {A, B} = lists:split(Head, Elements),
+  B ++ A.
+
+-spec length(Ring :: ring()) -> integer().
+length(#ring{length = Len}) ->
+   Len.
+
+%% @doc Shift the head of the ring `N' steps. Negative means
+%% leftwards, i.e. removing N elements from the head and inserting
+%% them at the end, and positive shifts means rightwards.
+-spec shift(N :: integer(), Ring :: ring()) -> ring().
+shift(0, Ring) -> Ring;
+shift(N, #ring{head = Head, length = Len} = Ring) ->
+  NewHead =
+    case (Head - N) rem Len of
+      H when H < 0 -> Len + H;
+      H -> H
+    end,
+  Ring#ring{head = NewHead}.
+
+-spec move_element_to_head(Elem :: term(), Ring :: ring()) -> ring().
+move_element_to_head(Elem, #ring{elements = Elements} = Ring) ->
+  Ring#ring{head = find_elem(Elem, Elements, 0)}.
+
+find_elem(Elem, [Elem|_], Idx) ->
+  Idx;
+find_elem(Elem, [_|Elems], Idx) ->
+  find_elem(Elem, Elems, Idx + 1).
+
+-spec peek(Ring :: ring()) -> term().
+peek(#ring{head = Head, elements = Elems}) ->
+  lists:nth(Head + 1, Elems).
+
+%% Insert an element at the head. All elements are moved one step
+%% rightwards.
+-spec insert(Element :: term(), Ring :: ring()) -> ring().
+insert(Element, #ring{head = Head,
+                      length = Length,
+                      elements = Elements}) ->
+  {A, B} = lists:split(Head, Elements),
+  #ring{head = Head,
+        length = Length + 1,
+        elements = A ++ [Element] ++ B}.
+
+-spec remove(Ring :: ring()) -> {Element :: term(), Ring0 :: ring()}.
+remove(#ring{head = Head,
+             length = Length,
+             elements = Elements}) ->
+  {A, [HeadElem|B]} = lists:split(Head, Elements),
+  {HeadElem, #ring{head = Head, length = Length - 1, elements = A ++ B}}.
+
+
+-ifdef(TEST).
+shift_test() ->
+  %% Positive, i.e. rightwards
+  ?assertEqual([4,1,2,3], to_list(shift(1, from_list([1,2,3,4])))),
+  %% Negative, i.e. leftwards
+  ?assertEqual([2,3,4,1], to_list(shift(-1, from_list([1,2,3,4])))).
+
+shift2_test() ->
+  Ring = from_list([1,2,3,4,5]),
+  lists:foreach(fun(Offset) ->
+                    R0 = shift(-Offset, shift(Offset, Ring)),
+                    ?assertEqual(to_list(R0), to_list(Ring))
+                end, lists:seq(-100, 100)).
+
+insert_test() ->
+  R = from_list([1,2,3,4]),
+  R0 = shift(-2, R),
+  R1 = insert(42, R0),
+  R2 = shift(2, R1),
+  ?assertEqual([1,2,42,3,4], to_list(R2)).
+
+remove_test() ->
+  R = from_list([1,2,3,4]),
+  {1, R0} = remove(R),
+  ?assertEqual([2,3,4], to_list(R0)).
+
+-endif.
